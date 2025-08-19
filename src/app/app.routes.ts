9c4d4871a56d71daf973e9923c104138f04e1bab
santiwg <santiwgwuri@gmail.com>
2025-08-18 23:34:17 -0300

    Initial commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,76 @@
+import { Routes } from '@angular/router';
+import { TemplatePage } from './pages/template/template-page';
+import { HomePage } from './pages/home/home-page';
+import { SalesPage } from './pages/sales/sales-page';
+import { PurchasesPage } from './pages/purchases/purchases-page';
+import { CollectionsPage } from './pages/collections/collections-page';
+import { PaymentsPage } from './pages/payments/payments-page';
+import { SuppliersPage } from './pages/suppliers/suppliers-page';
+import { CustomersPage } from './pages/customers/customers-page';
+import { ProductionPage } from './pages/production/production-page';
+import { ProductsPage } from './pages/products/products-page';
+import { EmployeesPage } from './pages/employees/employees-page';
+import { CashFlowPage } from './pages/cash-flow/cash-flow-page';
+import { CifPage } from './pages/cif/cif-page';
+import { IngredientsPage } from './pages/ingredients/ingredients-page';
+
+export const routes: Routes = [
+  {
+    path: '',
+  component: TemplatePage,
+    children: [
+      {
+        path: '',
+  component: HomePage,
+      },
+      {
+        path: 'sales',
+  component: SalesPage
+      },
+      {
+        path: 'purchases',
+  component: PurchasesPage
+      },
+      {
+        path: 'collections',
+  component: CollectionsPage
+      },
+      {
+        path: 'payments',
+  component: PaymentsPage
+      },
+      {
+        path: 'suppliers',
+  component: SuppliersPage
+      },
+      {
+        path: 'customers',
+  component: CustomersPage
+      },
+      {
+        path: 'production',
+  component: ProductionPage
+      },
+      {
+        path: 'supplies',
+  component: IngredientsPage
+      },
+      {
+        path: 'products',
+  component: ProductsPage
+      },
+      {
+        path: 'employees',
+  component: EmployeesPage
+      },
+      {
+        path: 'cash-flow',
+  component: CashFlowPage
+      },
+      {
+        path: 'cif',
+  component: CifPage
+      }
+    ],
+  },
+];
---
+++ src/app/services/unit.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { axiosClient } from './axios-client';
+import { config } from '../config/env';
+import { Unit, UnitDto } from '../interfaces/unit.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UnitService {
+  private baseUrl = config.urls.unit;
+
+  constructor() { }
+
+  async getUnits(): Promise<{
+    success: true; data: Unit[]
+  } | { success: false; error: string }> {
+    try {
+      const response = await axiosClient.get(this.baseUrl);
+      return { success: true, data: response.data };
+    } catch (error: any) {
+      const message =
+        error.response?.data?.message ||  // si existe un mensaje de error que manda el backend lo uso
+        error.message ||                  // mensaje generado por Axios
+        "Unknown Error";                  // por si no se obtiene
+      return { success: false, error: message };
+    }
+  }
+
+  async createUnit(unit: UnitDto): Promise<{
+    success: true; data: Unit
+  } | { success: false; error: string }
+  > {
+    try {
+      const response = await axiosClient.post(
+        this.baseUrl,
+        unit
+      );
+
+      return { success: true, data: response.data }
+
+    } catch (error: any) {
+      const message =
+        error.response?.data?.message ||  // si existe un mensaje de error que manda el backend lo uso
+        error.message ||                  // mensaje generado por Axios
+        "Unknown Error";                  // por si no se obtiene
+      return { success: false, error: message };
+    }
+  }
+  async getUnitsByScope(scope: string): Promise<{
+    success: true; data: Unit[]
+  } | { success: false; error: string }> {
+    try {
+      const response = await axiosClient.get(`${this.baseUrl}/find-by-scope/${scope}`);
+      return { success: true, data: response.data };
+    } catch (error: any) {
+      const message =
+        error.response?.data?.message ||  // si existe un mensaje de error que manda el backend lo uso
+        error.message ||                  // mensaje generado por Axios
+        "Unknown Error";                  // por si no se obtiene
+      return { success: false, error: message };
+    }
+  }
+  // Métodos CRUD aquí
+}
